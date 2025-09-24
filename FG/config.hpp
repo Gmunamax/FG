e7ae5a869d4b342a0eddac30482c1869e19897a3
Gmunamax <gmunamax@gmail.com>
2025-09-24 19:23:12 +0300

    FG separation

--- FG/config.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#define WINDOW1W 1280
-#define WINDOW1H 720
-
-#define WINDOW2W 320
-#define WINDOW2H 240
--- FG/resources/shaders/sth.frag
+++
@@ -1,8 +0,0 @@
-#version 460
-
-out vec4 rescolor;
-in vec4 color;
-
-void main(){
-	rescolor = color;
-}
