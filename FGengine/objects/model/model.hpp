ba7ee99143d1460b6d0edbcf67a2ae892ed81afa
Gmunamax <gmunamax@gmail.com>
2025-09-27 17:57:11 +0300

    fix building

--- FGengine/objects/model/model.hpp
+++ FGengine/objects/model/model.hpp
@@ -6,8 +6,8 @@
 template<typename VertexType>
 class Model: public Transform<typename VertexType::VertexPosition::DataType>, public Drawer<VertexType>{
 
-	VertexType::VertexPosition::DataType collidepos;
-	VertexType::VertexPosition::DataType collidescale;
+	typename VertexType::VertexPosition::DataType collidepos;
+	typename VertexType::VertexPosition::DataType collidescale;
 
 public:
 	Model(){};
